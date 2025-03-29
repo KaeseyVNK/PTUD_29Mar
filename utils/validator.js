02bfa831d0c4f073976acad36eff1e2a8620431d
KaeseyVNK <votrungkien3002@gmail.com>
2025-03-29 09:43:26 +0700

    nop bai

---
+++ utils/validator.js
@@ -0,0 +1,66 @@
+let { body, validationResult } = require('express-validator')
+let constants = require('./constants')
+let util = require('util')
+
+let options = {
+    password:{
+        minLength: 8,
+        minLowercase: 1,
+        minUppercase: 1,
+        minNumbers: 1,
+        minSymbols: 1
+    },
+    username:{
+        minLength: 6
+    }
+}
+
+module.exports = {
+    validate: function (req, res, next) {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            CreateErrorResponse(res, 400, errors.array())
+        } else {
+            next();
+        }
+    },
+    SignUpValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL)
+    ],
+    LoginValidator: [
+        body("username").isLength(options.username).withMessage("username hoac password sai"),
+        body("password").isStrongPassword(options.password).withMessage("username hoac password sai")
+    ],
+    CreateUserValidator: [
+        body("username").isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("password").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols)),
+        body("email").isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL),
+        body("role").isIn(['user', 'admin']).withMessage("Phai la user hoac admin")
+    ],
+    UpdateUserValidator: [
+        body("username").optional().isLength(options.username).withMessage(util.format(constants.VALIDATOR_ERROR_USERNAME,options.username.minLength)),
+        body("email").optional().isEmail().withMessage(constants.VALIDATOR_ERROR_EMAIL),
+        body("role").optional().isIn(['user', 'admin']).withMessage("Phai la user hoac admin")
+    ],
+    ChangePasswordValidator: [
+        body("oldpassword").notEmpty().withMessage("Old password is required"),
+        body("newpassword").isStrongPassword(options.password).withMessage(util.format(constants.VALIDATOR_ERROR_PASSWORD,
+            options.password.minLength,
+            options.password.minLowercase,
+            options.password.minUppercase,
+            options.password.minNumbers,
+            options.password.minSymbols))
+    ]
+}
